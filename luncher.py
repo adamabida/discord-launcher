dcad5532d8cf495359f9288445b447938e0c9375
Adam Abida <adamabida20@gmail.com>
2025-09-28 22:09:18 +0100

    Initial commit:Discord Launcher script

---
+++ luncher.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+import psutil
+import time
+from datetime import datetime
+import ctypes
+
+# --- Settings ---
+DISCORD_PATH = r"C:\Users\adama\AppData\Local\Discord\Update.exe"
+
+def show_warning(message):
+    ctypes.windll.user32.MessageBoxW(0, message, "Warning", 1)
+
+def is_discord_running():
+    for proc in psutil.process_iter(attrs=['name']):
+        if "Discord" in proc.info['name']:
+            return True
+    return False
+
+
+def get_allowed_start():
+    day = datetime.now().weekday()
+    if day <= 4 :   # Monday–Friday
+        return 21  # midnight
+    else:          # Saturday–Sunday
+        return 0   # 01:00
+       
+
+def launch_discord():
+    subprocess.Popen([DISCORD_PATH, "--processStart", "Discord.exe"])
+
+def main():
+    now = datetime.now()
+    AllOWED_END = 24
+    ALLOWED_START = get_allowed_start()
+    if ALLOWED_START <= now.hour < AllOWED_END :
+        print('Opening')
+        launch_discord()        
+    else:
+        show_warning("Maybe later. Discord is only available between "
+                     f"{ALLOWED_START}:00 and {AllOWED_END}:00")
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,32 @@
+\# Discord Launcher
+
+
+
+A Python script that controls when you can open Discord.
+
+
+
+\## Rules
+
+\- Weekdays: allowed from 21:00 until midnight.
+
+\- Weekends: allowed all day.
+
+\- Outside these hours, a popup will say "Maybe later".
+
+(You can change the time based on your preferred usage)
+
+
+
+\## How to use
+
+1\. Install Python.
+
+2\. Update the `DISCORD\_PATH` in `launcher.py` with your own Discord path.
+
+3\. Run the script:
+
+&nbsp;  ```bash
+
+&nbsp;  python launcher.py
+
